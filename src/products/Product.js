b9636c5c8cfafcba16e1b74d43459b9d0b9a8557
Fachri Naufal <fachrinfl@gmail.com>
2017-12-04 13:36:29 +0700

    Update

--- src/products/Product.js
+++ src/products/Product.js
@@ -5,10 +5,11 @@ import {
     TouchableOpacity,
     ActivityIndicator,
     Image,
-    ScrollView
+    ScrollView,
+    AsyncStorage
 } from 'react-native'
 import axios from 'axios'
-import { BASE_API_URL } from './global/util'
+import { BASE_API_URL, getToken } from '../global/util'
 import ProductList from './ProductList'
 
 
@@ -21,14 +22,31 @@ class Product extends Component {
     }
 
     componentDidMount(){
-        this.getProducts()
+        // jika token masih tersangkut dilokal gunakan AsyncStorage.removeItem('key token')
+        // AsyncStorage.removeItem('token')
+        this._getProducts()
     }
 
+    // props di bisa dipakai ketika ada 'class'
+    // method axios.post harus tetap mengirim data walau kosong '{}'
 
-    getProducts = () => {
-        console.log('function getProducts dipanggil')
+    _clickLogoutHandler = async () => {
+        const dataToken = await getToken()
+        console.log(dataToken)
+        const logout_api = `${BASE_API_URL}/Users/logout?access_token=${dataToken}`
+        console.log(logout_api)
+        axios.post(logout_api, {})
+            .then(response => {
+                AsyncStorage.removeItem('token')
+                this.props.navigation.navigate('Main')
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
 
-        axios.get(BASE_API_URL + '/shoppinglists')
+    _getProducts = () => {
+        axios.get(`${BASE_API_URL}/shoppinglists`)
             .then(response => {
                 this.setState({
                     dataProducts: response.data,
@@ -39,23 +57,36 @@ class Product extends Component {
             })
     }
 
-
     render(){
         return (
-          <ScrollView>
-            {this.state.dataProducts.length === 0 ? (
-              <ActivityIndicator
-                style={{ marginTop: 50 }}
-                size="large"/>) : (
-                  this.state.dataProducts.map((dataku, index) =>
-                   <ProductList
-                       key = {index}
-                       data = {dataku}
-                   />
-                  )
-                )}
-          </ScrollView>
+            <View style={{ flex: 1}}>
+                <ScrollView style = {{ padding: 10 }}>
+                    {this.state.dataProducts.length === 0 ? (
+                        <ActivityIndicator
+                            style={{ marginTop: 50 }}
+                            size="large"/>) : (
+                        this.state.dataProducts.map((dataku, index) =>
+                            <ProductList
+                                key = {index}
+                                data = {dataku}
+                                navigation = {this.props.navigation}
+                            />
+                        )
+                    )}
+                </ScrollView>
+                <View>
+                    <TouchableOpacity
+                        onPress = { () => this._clickLogoutHandler()}
+                        style={{ backgroundColor: '#FF395E', justifyContent: 'center', alignItems: 'center' }}>
+                        <Text style={{ color: '#ffffff', margin: 20, fontWeight: 'bold', fontSize: 18 }}>Logout</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         )
+    }
+
+    static navigationOptions = {
+        title: `Catalog`
     }
 }
 
--- src/products/ProductList.js
+++ src/products/ProductList.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 
 
 // Function based component
 const ProductList = (data) => {
     const dataHasil = data.data
+    const dataNavigation = data.navigation
     console.log(data.data)
     return(
-        <View style={{ backgroundColor: '#ffffff', margin: 10 }}>
-            <View style={{ flexDirection: 'row' }}>
-                <Image source={{ uri: dataHasil.image }}
-                       style={{ width: 100, height: 100, resizeMode: 'contain' }}
-                />
-                <View style={{ marginLeft: 15 }}>
-                    <Text style={{ marginTop: 20, fontSize: 20 }}>{dataHasil.product_name}</Text>
-                    <Text style={{ fontSize: 15 }}>{dataHasil.price}</Text>
+        <TouchableOpacity
+            onPress = {() => dataNavigation.navigate('ProductDetail', {id: dataHasil.id})}>
+            <View style={{ backgroundColor: '#ffffff', marginBottom: 10, borderRadius: 5 }}>
+                <View style={{ flexDirection: 'row' }}>
+                    <Image source={{ uri: dataHasil.image }}
+                           style={{ width: 100, height: 100, resizeMode: 'contain' }}
+                    />
+                    <View style={{ marginLeft: 15 }}>
+                        <Text style={{ marginTop: 20, fontSize: 20 }}>{dataHasil.product_name}</Text>
+                        <View style ={{ flexDirection: 'row' }}>
+                            <Text style={{ fontSize: 15 }}>Rp. {(dataHasil.price).toLocaleString('id')}</Text>
+                            {
+                                dataHasil.price >= 15000000 ? (
+                                    <Text style = {{ color: 'red', marginLeft: 25 }}>15% Off</Text>
+                                ) : (
+                                    <Text style = {{ color: 'red', marginLeft: 25 }}>5% Off</Text>
+                                )
+                            }
+                        </View>
+                    </View>
                 </View>
             </View>
-        </View>
+        </TouchableOpacity>
     )
 }
 
